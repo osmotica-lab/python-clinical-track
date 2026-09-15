baffd86a09d85572c872c6b42b3a399af6ec2266
osmotica-lab <pilar.sancho@osmotica.es>
2026-09-15 12:20:01 +0200

    Primer commit: hola mundo y variables

---
+++ ejercicio.py
@@ -0,0 +1,11 @@
+edad = 49
+print(edad, type(edad))
+
+peso = 85.5
+print(peso, type(peso))
+
+nombre = "Alberto"
+print(nombre, type(nombre))
+
+ingresado = False
+print(ingresado, type(ingresado))
---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hola, mundo")
+edad = 25
+print(edad, type(edad))
+
+temperatura = 36.6
+print(temperatura, type(temperatura))
+
+nombre = "Ana"
+print(nombre, type(nombre))
+
+edad_paciente = 49
+print(edad_paciente, type(edad_paciente))
+
+peso_paciente = 85.5
+print(peso_paciente, type(peso_paciente))
+
+nombre_paciente = "Alberto"
+print(nombre_paciente, type(nombre_paciente))
+
+esta_hospitalizado = True
+print(esta_hospitalizado, type(esta_hospitalizado))
+
